c1083dbb5c0c199c0747f834d87c4bf3a1285b51
Jack Bendtsen <jackdbendtsen@gmail.com>
2024-09-19 03:22:07 +0000

    Added to drawing routines

--- main.go
+++ main.go
@@ -16,9 +16,60 @@ type MainMenu struct {
 
 type Game struct {
 	menu MainMenu
+	wndWidth int32
+	wndHeight int32
+	tileSize int32
 }
 
-func drawMenu(menu *MainMenu, isActive bool) {
+const NORMAL = 0
+const DOUBLE_WORD = 1
+const TRIPLE_WORD = 2
+const DOUBLE_LETTER = 3
+const TRIPLE_LETTER = 4
+
+var boardTileTypeLookup = [...]int32 {
+    2, 0, 0, 3, 0, 0, 0, 2,
+    0, 1, 0, 0, 0, 4, 0, 0,
+    0, 0, 1, 0, 0, 0, 3, 0,
+    3, 0, 0, 1, 0, 0, 0, 3,
+    0, 0, 0, 0, 1, 0, 0, 0,
+    0, 4, 0, 0, 0, 4, 0, 0,
+    0, 0, 3, 0, 0, 0, 3, 0,
+}
+
+var boardTileColorsRGBA = [...]uint32 {
+    0,
+    0xffe020ff,
+    0xe00000ff,
+    0x80e0ffff,
+    0x0040ffff,
+}
+
+func getTileType(x, y int32) int32 {
+    if x < 0 || y < 0 || x >= 15 || y >= 15 {
+        return NORMAL
+    }
+    if x == 7 && y == 7 {
+        return DOUBLE_WORD
+    }
+
+    if x >= 7 && y >= 8 {
+        x = 15 - x
+        y = 15 - y
+    } else if x >= 8 && y <= 7 {
+        temp := x
+        x = y
+        y = 15 - temp
+    } else if x <= 7 && y >= 7 {
+        temp := x
+        x = 15 - y
+        y = temp
+    }
+
+    return boardTileTypeLookup[x + 8 * y]
+}
+
+func drawMenu(game *Game, isActive bool) {
 	
 }
 
@@ -26,19 +77,60 @@ func drawFallingBoard(t float32) {
 	
 }
 
-func drawGame(game *Game) {
-	
+func drawGame(game *Game, boardTex rl.Texture2D) {
+    boardLen := game.tileSize * 15
+    var xOff int32 = int32(game.wndWidth - boardLen) / 2
+    var yOff int32 = int32(game.wndHeight - boardLen) / 2
+	rl.DrawTexture(boardTex, xOff, yOff, rl.White)
 }
 
-func createBoard(tex *rl.Texture2D, canvas *rl.Image, wndWidth, wndHeight, oldTileSize int) (tileSize int) {
-	tileSize = wndWidth / 17
-	canvas = rl.CreateImage
+func maybeRecreateBoard(tex *rl.Texture2D, wndWidth, wndHeight, oldTileSize int32) (tileSize int32) {
+	tileSize = int32(min(wndWidth / 16, wndHeight / 18))
+    if tileSize == oldTileSize {
+        return tileSize
+    }
 
-	rl.UnloadImage(canvas)
+    boardLen := int(tileSize * 15)
+	canvas := rl.GenImageColor(boardLen, boardLen, color.RGBA{0, 192, 64, 255})
 
-	if tex.id != 0 {
-		rl.UnloadTexture(tex)
+    topTri    := Triangle{tileSize / 2, -tileSize / 6, 2 * tileSize / 3, 0, tileSize / 3, 0}
+    leftTri   := Triangle{-tileSize / 6, tileSize / 2, 0, tileSize / 3, 0, 2 * tileSize / 3}
+    rightTri  := Triangle{tileSize, tileSize / 3, 7 * tileSize / 6, tileSize / 2, tileSize, 2 * tileSize / 3}
+    bottomTri := Triangle{tileSize / 3, tileSize, 2 * tileSize / 3, tileSize, tileSize / 2, 7 * tileSize / 6}
+
+    // draw tiles
+    for y := int32(0); y < 15; y++ {
+        for x := int32(0); x < 15; x++ {
+            tt := getTileType(x, y)
+            if tt != 0 {
+                rgba := boardTileColorsRGBA[tt]
+                color := color.RGBA{uint8((rgba >> 24) & 0xff), uint8((rgba >> 16) & 0xff), uint8((rgba >> 8) & 0xff), uint8(rgba & 0xff)}
+                rl.ImageDrawRectangle(canvas, x * tileSize, y * tileSize, tileSize, tileSize, color)
+                renderTriangle(canvas.Data, tileSize * 15, tileSize * 15, x * tileSize, y * tileSize, &topTri, rgba)
+                renderTriangle(canvas.Data, tileSize * 15, tileSize * 15, x * tileSize, y * tileSize, &leftTri, rgba)
+                renderTriangle(canvas.Data, tileSize * 15, tileSize * 15, x * tileSize, y * tileSize, &rightTri, rgba)
+                renderTriangle(canvas.Data, tileSize * 15, tileSize * 15, x * tileSize, y * tileSize, &bottomTri, rgba)
+            }
+        }
+    }
+
+    // draw gridlines
+    lineColor := color.RGBA{224, 224, 255, 255}
+    lineWidth := tileSize / 8
+    lineOff := tileSize - (lineWidth / 2)
+    for i := int32(0); i < 14; i++ {
+        rl.ImageDrawRectangle(canvas, 0, i * tileSize + lineOff, tileSize * 15, lineWidth, lineColor)
+        rl.ImageDrawRectangle(canvas, i * tileSize + lineOff, 0, lineWidth, tileSize * 15, lineColor)
+    }
+
+	if tex.ID != 0 {
+		rl.UnloadTexture(*tex)
 	}
+
+    *tex = rl.LoadTextureFromImage(canvas)
+    rl.UnloadImage(canvas)
+
+    return tileSize
 }
 
 func loadWords() []string {
@@ -67,38 +159,37 @@ func main() {
 	rl.InitWindow(800, 450, "scrambles")
 	defer rl.CloseWindow()
 
-	wndWidth := 0
-	wndHeight := 0
-
 	rl.SetTargetFPS(60)
 
 	game := Game{}
 	boardTex := rl.Texture2D{}
-	boardImage := rl.Image{}
 
-	openingTimer := 0
+	//openingTimer := 0
 	const maxOpeningTime = 240
 
 	for !rl.WindowShouldClose() {
+	    w := int32(rl.GetRenderWidth())
+		h := int32(rl.GetRenderHeight())
+		if game.wndWidth != w || game.wndHeight != h {
+			game.wndWidth = w
+			game.wndHeight = h
+			game.tileSize = maybeRecreateBoard(&boardTex, w, h, game.tileSize)
+		}
+
 		rl.BeginDrawing()
 		rl.ClearBackground(color.RGBA{0, 0x68, 0x3a, 0xff})
 
-		w := rl.GetRenderWidth()
-		h := rl.GetRenderHeight()
-		if wndWidth != w || wndHeight != h {
-			wndWidth = w
-			wndHeight = h
-			createBoard(&boardTex, &boardImage, w, h)
-		}
-
+        /*
 		if openingTimer < maxOpeningTime {
-			drawMenu(&game.menu, openingTimer == 0)
+			drawMenu(&game, openingTimer == 0)
 			if openingTimer > 0 {
 				drawFallingBoard(float32(openingTimer) / float32(maxOpeningTime))
 			}
 		} else {
 			drawGame(&game)
 		}
+		*/
+		drawGame(&game, boardTex)
 
 		rl.EndDrawing()
 	}
